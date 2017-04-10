41f2a06b87fcbfca2dbf8ddc4185e0ee4e6d81a0
Kelly Selden <kellyselden@gmail.com>
2017-04-10 12:25:11 -0700

    use eslint-config-sane

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-var expect = require('chai').expect;
-var transformFileSync = require('babel').transformFileSync;
+const path = require('path');
+const fs = require('fs');
+const expect = require('chai').expect;
+const transformFileSync = require('babel').transformFileSync;
 
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
 }
 
-var fixturesDir = path.join(__dirname, 'fixtures');
+let fixturesDir = path.join(__dirname, 'fixtures');
 
-var testCount = 0;
+let testCount = 0;
 
 function test(caseName) {
-  var testName = caseName.split('-').join(' ');
-  var fixtureDir = path.join(fixturesDir, caseName);
-  var actualPath = path.join(fixtureDir, 'actual.js');
-  var expectedPath = path.join(fixtureDir, 'expected.js');
-  var actual;
-  var expected;
+  let testName = caseName.split('-').join(' ');
+  let fixtureDir = path.join(fixturesDir, caseName);
+  let actualPath = path.join(fixtureDir, 'actual.js');
+  let expectedPath = path.join(fixtureDir, 'expected.js');
+  let actual;
+  let expected;
   try {
     actual = transformFileSync(actualPath).code;
     expected = fs.readFileSync(expectedPath).toString()
@@ -41,9 +41,9 @@ function test(caseName) {
 }
 
 describe('rewrites imports', function() {
-  var caseNames = fs.readdirSync(fixturesDir);
+  let caseNames = fs.readdirSync(fixturesDir);
 
-  var only = caseNames.filter(function(caseName) {
+  let only = caseNames.filter(function(caseName) {
     return caseName.indexOf('_') === 0;
   });
   if (only.length) {

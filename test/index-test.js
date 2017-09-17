f2bfe9ef9937907be15ba730d390a6375c889510
Kelly Selden <kellyselden@gmail.com>
2017-09-17 10:10:16 -0700

    use fixture-skipper

--- test/index-test.js
+++ test/index-test.js
@@ -3,9 +3,12 @@
 const path = require('path');
 const fs = require('fs');
 const expect = require('chai').expect;
+const fixtureSkipper = require('fixture-skipper');
 const transformFileSync = require('babel').transformFileSync;
 
 const fixturesDir = path.join(__dirname, 'fixtures');
+
+const forEachDir = fixtureSkipper(fixturesDir);
 
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
@@ -13,44 +16,33 @@ function trim(str) {
 
 let testCount = 0;
 
-function test(caseName) {
-  let testName = caseName.split('-').join(' ');
-  let fixtureDir = path.join(fixturesDir, caseName);
-  let actualPath = path.join(fixtureDir, 'actual.js');
-  let expectedPath = path.join(fixtureDir, 'expected.js');
-  let actual;
-  let expected;
-  try {
-    actual = transformFileSync(actualPath).code;
-    expected = fs.readFileSync(expectedPath).toString()
-      .replace(/\r\n/g, '\n');
-  } catch (err) {
-    if (err._babel) {
-      throw err;
+describe('rewrites imports', function() {
+  forEachDir((it, caseName) => {
+    let testName = caseName.split('-').join(' ');
+    let fixtureDir = path.join(fixturesDir, caseName);
+    let actualPath = path.join(fixtureDir, 'actual.js');
+    let expectedPath = path.join(fixtureDir, 'expected.js');
+    let actual;
+    let expected;
+    try {
+      actual = transformFileSync(actualPath).code;
+      expected = fs.readFileSync(expectedPath).toString()
+        .replace(/\r\n/g, '\n');
+    } catch (err) {
+      if (err._babel) {
+        throw err;
+      }
+
+      // empty folders
+      return;
     }
 
-    // empty folders
-    return;
-  }
+    testCount++;
 
-  testCount++;
-
-  it(`should ${testName}`, function() {
-    expect(trim(actual)).to.equal(trim(expected));
+    it(`should ${testName}`, function() {
+      expect(trim(actual)).to.equal(trim(expected));
+    });
   });
-}
-
-describe('rewrites imports', function() {
-  let caseNames = fs.readdirSync(fixturesDir);
-
-  let only = caseNames.filter(function(caseName) {
-    return caseName.indexOf('_') === 0;
-  });
-  if (only.length) {
-    caseNames = only;
-  }
-
-  caseNames.forEach(test);
 
   if (!testCount) {
     throw new Error('no tests were run');
